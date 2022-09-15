1acb30f8c5eb325109b3320cc94706d6e797e0a7
ugo-bee <ugoafav@gmail.com>
2022-09-15 12:35:36 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#given a list of integers, create a function that returns the nth-rarest item.the funtion should 
+#be called nth_most_rate signature and its nth_most_rate signature(list,n) where list is the array of 
+#integers and n is the nth rarest item.For Example in([5,4,5,4,5,4,4,5,3,3,3,2,2,1,5]),if 2 is supplied as n
+#the answer is 2 as 2 is the second rarest item
+
