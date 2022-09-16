c8ed21d898cb9c3d171959042b0cf3892a0b4403
ugo-bee <ugoafav@gmail.com>
2022-09-16 16:30:49 -0700

    comments

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+
+/*
+Given a list of integers, create a function that returns the nth-rarest item.
+The funtion should be called nth_most_rate signature and its nth_most_rate signature(list,n),
+Where list is the array of integers and n is the nth rarest item.
+For Example in([5,4,5,4,5,4,4,5,3,3,3,2,2,1,5]),if 2 is supplied as n
+the answer is 2 as 2 is the second rarest item.
+*/
+
+
+
+
+
 function nth_most_rate(list,n){
-    let unique_numbers_list = []
+    // unique numbers
     const unique_numbers = list.filter((value,index,self)=> self.indexOf(value)===index)
       let unique_items_count =[]
-  
+    // count of  unique numbers
       for (let i = 0; i < unique_numbers.length; i += 1) {
       const uniqueNumber = unique_numbers[i]
   
@@ -13,7 +26,8 @@ function nth_most_rate(list,n){
   
       const arrangedByCount = unique_items_count.sort((first, second) => first.count - second.count);
       return arrangedByCount[n - 1].uniqueNumber;
-}
-  const list = [1, 1, 1, 2, 2, 5, 5, 6, 4, 4];
-  const n = 1;
+} 
+    // Testing the code,to check if returns the nth_most_rate item
+  const list = [5,4,5,4,5,4,4,5,3,3,3,2,2,1,5];
+  const n = 2;
   console.log(nth_most_rate(list, n));
